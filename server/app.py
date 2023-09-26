ea0c523ef7e47cca1df50766da84493d3e206473
Cnnjuguna <123736824+Cnnjuguna@users.noreply.github.com>
2023-09-26 14:27:37 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -1,34 +1,61 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from urllib import response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return '<h1>Zoo app</h1>'
+    return "<h1>Zoo app</h1>"
 
-@app.route('/animal/<int:id>')
+
+@app.route("/animal/<int:id>")
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    response_body = f"""
+        <ul>{animal.name}</ul>
+        <ul>{animal.species}</ul>
+        <ul>{animal.zookeeper}</ul>
+        <ul>{animal.enclosure}</ul>
+    """
+    response = make_response(jsonify(response_body), 200)
+    return response
 
-@app.route('/zookeeper/<int:id>')
+
+@app.route("/zookeeper/<int:id>")
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Animal.query.filter(Animal.id == id).first()
+    response_body = f"""
+        <ul>{zookeeper.name}</ul>
+        <ul>{zookeeper.enclosure}</ul>
+        <ul>{zookeeper.enclosure}</ul>
+    """
+    response = make_response(jsonify(response_body), 200)
+    return response
 
-@app.route('/enclosure/<int:id>')
+
+@app.route("/enclosure/<int:id>")
 def enclosure_by_id(id):
-    return ''
+    enclosure = Animal.query.filter(Animal.id == id).first()
+    response_body = f"""
+        <ul>{enclosure.enclosure}</ul>
+        <ul>{enclosure.open_to_visitors}</ul>
+        <ul>{enclosure.animals.all()}</ul>
+    """
+    response = make_response(jsonify(response_body), 200)
+    return response
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,37 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
+    __tablename__ = "zookeepers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    birthday = db.Column(db.Integer(), nullable=False)
+    animals = db.Column(db.String())
+
 
 class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
+    __tablename__ = "enclosures"
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String())
+    open_to_visitors = db.Column(db.Boolean())
+
 
 class Animal(db.Model):
-    __tablename__ = 'animals'
+    __tablename__ = "animals"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    species = db.Column(db.String(), nullable=False)
+    zookeeper = db.Column(db.String())
+    enclosure = db.Column(db.String())
